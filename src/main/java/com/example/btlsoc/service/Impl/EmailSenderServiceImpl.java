cd1390dbeba6aacd9e8e880bfcb0679ef53d9312
Hoàng <hoangnv2810@gmail.com>
2023-04-24 17:21:45 +0700

    send email - Đạt

---
+++ src/main/java/com/example/btlsoc/service/Impl/EmailSenderServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.btlsoc.service.Impl;
+
+import com.example.btlsoc.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderServiceImpl implements EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+
+    @Override
+    public void sendEmail(String to, String subject, String message) {
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+        simpleMailMessage.setFrom("hoangnv2810@gmail.com");
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(message);
+
+        mailSender.send(simpleMailMessage);
+    }
+}
